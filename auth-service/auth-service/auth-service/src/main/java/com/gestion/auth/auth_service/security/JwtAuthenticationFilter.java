e48a938f35bb72feba46cd5a717afc231bca7051
Axel <dsg4857@gmail.com>
2026-05-27 18:18:04 -0500

    add: Crear Servicio y filtro de autentication de JWT

---
+++ auth-service/auth-service/auth-service/src/main/java/com/gestion/auth/auth_service/security/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.gestion.auth.auth_service.security;
+
+public class JwtAuthenticationFilter {
+}
---
+++ auth-service/auth-service/auth-service/src/main/java/com/gestion/auth/auth_service/security/JwtService.java
@@ -0,0 +1,72 @@
+package com.gestion.auth.auth_service.security;
+
+
+import com.gestion.auth.auth_service.credential.entity.Credencial;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpiration;
+
+    public String generateToken(Credencial credential) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + jwtExpiration);
+
+        return Jwts.builder()
+                .subject(credential.getCorreo())
+                .claim("userId", credential.getUserId())
+                .claim("role", credential.getRol().getNombre())
+                .issuedAt(now)
+                .expiration(expirationDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractCorreo(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Long extractUserId(String token) {
+        return extractAllClaims(token).get("userId", Long.class);
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public boolean isTokenValid(String token) {
+        return !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = jwtSecret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
