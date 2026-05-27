a983c9893a4f43cec81da1e7342b13e4d1f2820d
Axel <dsg4857@gmail.com>
2026-05-27 18:19:07 -0500

    add: Crear controlador y servicio de Authentication

---
+++ auth-service/auth-service/auth-service/src/main/java/com/gestion/auth/auth_service/auth/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.gestion.auth.auth_service.auth.controller;
+
+import com.gestion.auth.auth_service.auth.dto.CrearCredencialRequest;
+import com.gestion.auth.auth_service.auth.dto.CredencialResponse;
+import com.gestion.auth.auth_service.auth.dto.LoginRequest;
+import com.gestion.auth.auth_service.auth.dto.LoginResponse;
+import com.gestion.auth.auth_service.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody LoginRequest request
+    ) {
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/credentials")
+    public ResponseEntity<CredencialResponse> createCredential(
+            @Valid @RequestBody CrearCredencialRequest request
+    ) {
+        CredencialResponse response = authService.createCredential(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
---
+++ auth-service/auth-service/auth-service/src/main/java/com/gestion/auth/auth_service/auth/service/AuthService.java
@@ -0,0 +1,78 @@
+package com.gestion.auth.auth_service.auth.service;
+
+import com.gestion.auth.auth_service.auth.dto.CrearCredencialRequest;
+import com.gestion.auth.auth_service.auth.dto.CredencialResponse;
+import com.gestion.auth.auth_service.auth.dto.LoginRequest;
+import com.gestion.auth.auth_service.auth.dto.LoginResponse;
+import com.gestion.auth.auth_service.credential.entity.Credencial;
+import com.gestion.auth.auth_service.credential.repository.CredencialRepository;
+import com.gestion.auth.auth_service.role.entity.Rol;
+import com.gestion.auth.auth_service.role.repository.RolRepository;
+import com.gestion.auth.auth_service.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final CredencialRepository credencialRepository;
+    private final RolRepository rolRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public LoginResponse login(LoginRequest request){
+        Credencial credencial = credencialRepository.findByCorreo(request.correo()).orElseThrow(() -> new BadCredentialsException("Credenciales invalidas"));
+
+        if(!credencial.getActivo()){
+            throw new BadCredentialsException("La cuenta no está inhabilitada");
+        }
+        boolean passwordValido = passwordEncoder.matches(request.password(), credencial.getPassword());
+
+        if(!passwordValido) {
+            throw new BadCredentialsException("Credenciales inválidas");
+        }
+        String token = jwtService.generateToken(credencial);
+
+        return new LoginResponse(token, "Bearer", credencial.getUserId(), credencial.getCorreo(), credencial.getRol().getNombre());
+
+    }
+
+    public CredencialResponse createCredential(CrearCredencialRequest request) {
+        if (credencialRepository.existsByCorreo(request.correo())) {
+            throw new IllegalArgumentException("El correo ya tiene credenciales registradas");
+        }
+
+        if (credencialRepository.existsByUserId(request.userId())) {
+            throw new IllegalArgumentException("El usuario ya tiene credenciales registradas");
+        }
+
+        Rol role = rolRepository.findByNombre(request.rol())
+                .orElseThrow(() -> new IllegalArgumentException("El rol no existe"));
+
+        Credencial credential = Credencial.builder()
+                .userId(request.userId())
+                .correo(request.correo())
+                .password(passwordEncoder.encode(request.password()))
+                .rol(role)
+                .activo(true)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Credencial savedCredential = credencialRepository.save(credential);
+
+        return new CredencialResponse(
+                savedCredential.getId(),
+                savedCredential.getUserId(),
+                savedCredential.getCorreo(),
+                savedCredential.getRol().getNombre(),
+                savedCredential.getActivo()
+        );
+    }
+
+
+}
